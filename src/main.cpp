4290c7cf1b18467ad49b2447c686a0b5ec45f5dc
akshay <akshay.skv1225@gmail.com>
2026-03-26 22:35:35 +0530

    Got the paj library working and did basic testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
+#include "DFRobot_PAJ7620U2.h"
 #include "includes/AudioController.h"
 
-AudioController con("obama", 22, 25, 26, 12);
+DFRobot_PAJ7620U2 sens;
+
+AudioController con("espSpeaker", 22, 25, 26, 12);
 
 void setup() {
+    Serial.begin(115200);
     con.init();
+    Wire.begin(18, 19);
+    while(sens.begin() != 0) {
+        Serial.println("Error initializing sensor!");
+    }
+
+    sens.setGestureHighRate(false);
 }
 
 void loop() {
-
+    auto ges = sens.getGesture();
+    if(ges != sens.eGestureNone) {
+        String desc = sens.gestureDescription(ges);
+        Serial.print("gesture: "); Serial.println(desc);
+    }
 }
